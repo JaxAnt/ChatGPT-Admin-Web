2d837744b6f84352bd47d41386e039c78d703a67
JaxAnt <94965319+JaxAnt@users.noreply.github.com>
2023-05-14 01:20:57 +0800

    Update page.tsx

--- apps/dash/src/app/[area]/page.tsx
+++ apps/dash/src/app/[area]/page.tsx
@@ -63,7 +63,7 @@ export default function Page({
       setCurrentPage(0);
       setCount(Math.abs(gap) * 10);
     }
-    setCount();
+    setCount(gap);
   };
 
   if (isLoading) return <Loading />;
